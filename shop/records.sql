a6a6e329c322e1e880c04d3548c211f52d60e3ef
Ruslan Farhiullin <farhiullin2001@mail.ru>
2024-05-31 00:10:44 +0300

    we added tables

--- shop/records.sql
+++ shop/records.sql
@@ -17,3 +17,32 @@ VALUES
     ('Microphones', (SELECT id FROM categories WHERE name = 'Electronics')),
     ('Automobile', NULL);
 COMMIT;
+
+INSERT INTO products
+    (name, category_id, price)
+VALUES
+    ('Паяльник', (SELECT id FROM categories WHERE name = 'Electronics'), 5000),
+    ('Светильник', (SELECT id FROM categories WHERE name = 'Electronics'), 4000),
+    ('Iphone 18+MAX', (SELECT id FROM categories WHERE name = 'Phones'), 999999.99),
+    ('Google Pixel 6', (SELECT id FROM categories WHERE name = 'Phones'), 39000),
+    ('Sony XBass', (SELECT id FROM categories WHERE name = 'Headphones'), 4000),
+    ('Marshals', (SELECT id FROM categories WHERE name = 'Headphones'), 4000),
+    ('DEXP', (SELECT id FROM categories WHERE name = 'Microphones'), 900),
+    ('Oklick', (SELECT id FROM categories WHERE name = 'Microphones'), 1100),
+    ('SUBARU IMPREZA', (SELECT id FROM categories WHERE name = 'Automobile'), 1000000000),
+    ('LADA 2113', (SELECT id FROM categories WHERE name = 'Automobile'), 1000000);
+
+INSERT INTO orders
+    (created_at, user_id, sum)
+VALUES
+    ('2024-05-30 23:05:55.524 +0300', (SELECT id FROM users WHERE name = 'Света'), 900),
+    ('2024-05-30 23:05:55.524 +0300', (SELECT id FROM users WHERE name = 'Руслан'), 500),
+    ('2024-05-30 23:05:55.524 +0300', (SELECT id FROM users WHERE name = 'Илья'), 1000);
+
+INSERT INT order_lines
+    (product_id, sum, amount, order_id)
+VALUES
+    ((SELECT id FROM products WHERE name = 'DEXP'), 900, 2, (SELECT id FROM orders WHERE sum  = 900)),
+    ((SELECT id FROM products WHERE name = 'Sony XBass'), 4000, 1, (SELECT id FROM orders WHERE sum = 1000)),
+    ((SELECT id FROM products WHERE name = 'Iphone 18+MAX'), 999999.99, 4, (SELECT id FROM orders WHERE sum = 500));
+
---
+++ shop/request.sql
@@ -0,0 +1,13 @@
+SELECT email FROM users;
+
+SELECT name FROM users;
+
+SELECT name, email FROM users;
+
+SELECT * FROM products WHERE price > 1000;
+
+SELECT * FROM users WHERE id = ANY (SELECT user_id FROM orders);
+
+SELECT * FROM products WHERE id = ANY (SELECT product_id FROM order_lines);
+
+SELECT * FROM categories WHERE id is NOT NULL;
